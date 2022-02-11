fed915e733a3c6cfb75d9de8213e1321a9a2f0cb
quanter-prog <quanter.box@gmail.com>
2022-02-11 20:11:31 +0300

    add QBXZ token based on openzeppelin ERC20 token library

---
+++ contracts/tokens/zeppelin/QBXZ.sol
@@ -0,0 +1,11 @@
+// contracts/token/zeppelin/QBXZ.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract QBXZ is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Qubix", "QBXZ") {
+        _mint(msg.sender, initialSupply);
+    }
+}
