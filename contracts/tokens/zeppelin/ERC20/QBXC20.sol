3841518cc3b87a4aec85ea402a44a6c3e646b79f
quanter-prog <quanter.box@gmail.com>
2022-03-03 22:05:56 +0300

    changed constructor for staking prototype contract

--- contracts/tokens/zeppelin/ERC20/QBXC20.sol
+++ contracts/tokens/zeppelin/ERC20/QBXC20.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract QBXC20 is ERC20 {
-    constructor(uint256 initialSupply) ERC20("QBXC20", "Q20") {
-        _mint(msg.sender, initialSupply);
+    constructor() ERC20("QBXC20", "Q20") {
+        _mint(msg.sender, 1000 * 10**uint256(decimals()));
     }
 }
