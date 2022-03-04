dafdd466e27c5dfe986430ed720c296963491a0d
quanter-prog <quanter.box@gmail.com>
2022-03-04 16:18:17 +0300

    changed constructor for interaction with staking smart-contract
    Signed-off-by: quanter-prog <quanter.box@gmail.com>

--- contracts/tokens/zeppelin/ERC20/QBXC20.sol
+++ contracts/tokens/zeppelin/ERC20/QBXC20.sol
@@ -3,8 +3,9 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract QBXC20 is ERC20 {
+contract QBXC20 is ERC20, Ownable {
     constructor() ERC20("QBXC20", "Q20") {
         _mint(msg.sender, 1000 * 10**uint256(decimals()));
     }
