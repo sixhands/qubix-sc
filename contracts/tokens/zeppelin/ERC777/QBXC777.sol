ef49b354e45f711140f8b63b1a21f750a6826132
quanter-prog <quanter.box@gmail.com>
2022-03-04 16:17:45 +0300

    fixed ERC777 deploy issue
    Signed-off-by: quanter-prog <quanter.box@gmail.com>

--- contracts/tokens/zeppelin/ERC777/QBXC777.sol
+++ contracts/tokens/zeppelin/ERC777/QBXC777.sol
@@ -1,13 +1,17 @@
-// contracts/token/zeppelin/ERC777/QBXC777.sol
+// conztracts/token/zeppelin/ERC777/QBXC777.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
 
 contract QBXC777 is ERC777 {
-    constructor(uint256 initialSupply)
-        ERC777("QBXC777", "Q777", new address[](0))
+    constructor(
+        string memory name,
+        string memory symbol,
+        address[] memory defaultOperators
+    )
+        ERC777(name, symbol, defaultOperators)
     {
-        _mint(msg.sender, initialSupply, "", "");
+        _mint(msg.sender, 10000 * 10 ** uint256(decimals()), "", "");
     }
 }
