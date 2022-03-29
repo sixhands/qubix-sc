bcfeb207a2c3a91811f9ee80326cfebac577530b
quanter-prog <quanter.box@gmail.com>
2022-03-30 01:54:26 +0300

    add market contract
    Signed-off-by: quanter-prog <quanter.box@gmail.com>

---
+++ contracts/tasks/Market.sol
@@ -0,0 +1,47 @@
+// contracts/tasks/Market.sol
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./QBXC.sol";
+import "./Item.sol";
+
+contract Market {
+
+    QBXC internal _QBXC;
+    Item internal _Item;
+
+    address public ekosystem;
+    modifier onlyEko() {
+        require(msg.sender == ekosystem, "only ekosystem");
+        _;
+    }
+
+    constructor(
+        QBXC tokenContract,
+        Item itemContract,
+        address ekosystemAddress) {
+
+        ekosystem = ekosystemAddress;
+        _QBXC = tokenContract;
+        _Item = itemContract;
+    }
+
+    function sellItem(
+        address seller,
+        address buyer,
+        uint256 itemPrice,
+        uint256 itemId) public returns (bool) {
+
+        require(seller == _Item.ownerOf(itemId), "seller is not an owner of the item");
+        require(_QBXC.balanceOf(buyer) >= itemPrice, "buyer doesn't have enough tokens");
+
+        _QBXC.increaseAllowance(buyer, itemPrice);
+        _QBXC.transferFrom(buyer, seller, itemPrice);
+
+        _Item.approve(buyer, itemId);
+        _Item.transferFrom(seller, buyer, itemId);
+
+        return true;
+    }
+}
