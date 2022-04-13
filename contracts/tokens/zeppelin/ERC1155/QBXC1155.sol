aa663cec6069bbaf3cbea9a25df5241c42bc5896
quanter-prog <quanter.box@gmail.com>
2022-04-13 18:24:48 +0300

    feature: add metadata to ERC1155, ERC1155 updated
    Signed-off-by: quanter-prog <quanter.box@gmail.com>

--- contracts/tokens/zeppelin/ERC1155/QBXC1155.sol
+++ contracts/tokens/zeppelin/ERC1155/QBXC1155.sol
@@ -1,19 +1,26 @@
 // contracts/token/zeppelin/ERC1155/ERC1155.sol
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract QBXC1155 is ERC1155 {
-    uint256 public constant CRYSTALS = 0;
-    uint256 public constant QBXC = 1;
-    uint256 public constant CUBE_FACE = 2;
-    uint256 public constant SKIN = 3;
+    //uint256 public constant QBXC = 0;
+    uint256 public constant SOME_ASSET = 1;
 
-    constructor() ERC1155("https://backend/api/item/{id}.json") {
-        _mint(msg.sender, CRYSTALS, 10**21, "");
-        _mint(msg.sender, QBXC, 10**21, "");
-        _mint(msg.sender, CUBE_FACE, 100, "");
-        _mint(msg.sender, SKIN, 10**9, "");
+    constructor() ERC1155("https://ipfs.io/ipfs/bafybeigfs3ikzbwsy7aopvzf6v4dms2tehz6pgsjxy2lyixdfjuzvmwm2a/{id}.json") {
+        //_mint(msg.sender, QBXC, 10**24, "");
+        _mint(msg.sender, SOME_ASSET, 1, "");
+    }
+
+    function uri(uint256 _tokenid) override public pure returns (string memory) {
+        return string(
+            abi.encodePacked(
+                "https://ipfs.io/ipfs/bafybeigfs3ikzbwsy7aopvzf6v4dms2tehz6pgsjxy2lyixdfjuzvmwm2a/",
+                Strings.toString(_tokenid),".json"
+            )
+        );
     }
 }
