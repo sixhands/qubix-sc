8a0f688393ee4bceda34fb022f97bca76d0f8e04
quanter-prog <quanter.box@gmail.com>
2022-04-15 02:32:29 +0300

    feature: added new ERC20 token and seed contract for them!
    Signed-off-by: quanter-prog <quanter.box@gmail.com>

---
+++ migrations/8_seed_deploy.js
@@ -0,0 +1,24 @@
+const QBXC = artifacts.require("QBXC");
+const SeedERC20 = artifacts.require("SeedPhaseERC20");
+
+module.exports = async function (deployer) {
+    await deployer.deploy(
+        QBXC
+    );
+
+    const qbxc = await QBXC.deployed()
+
+    const seed = await deployer.deploy(
+        SeedERC20,
+        qbxc.address,
+        [1000, 500, 250],
+        [
+            "0x32289C3688CEbf515A065E7D25DC5cEFDb6E9c12",
+            "0xCd552D7066da507FD9870a82A319953f713d542B",
+            "0xeC097800387fE89F22297f2Eebb386bAD9ed4CC5"
+        ]
+    );
+
+    await qbxc.setSeedRole(seed.address)
+    await seed.setSeedParametersToToken()
+};
