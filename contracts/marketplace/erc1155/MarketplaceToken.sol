667746827929bea6c43028f6b426f4d46a477116
quanter-prog <quanter.box@gmail.com>
2022-04-26 19:17:08 +0300

    feature: added prototype of marketplace contract
    Signed-off-by: quanter-prog <quanter.box@gmail.com>

---
+++ contracts/marketplace/erc1155/MarketplaceToken.sol
@@ -0,0 +1,49 @@
+// contracts/marketplace/erc1155/MarketplaceToken.sol
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract MarketplaceToken is ERC1155 {
+
+    uint256 public constant BLOODLOV = 1;
+    uint256 public constant LOMIX = 2;
+    uint256 public constant BULLER = 3;
+    uint256 public constant GOLDHUNTER = 4;
+    uint256 public constant POWERF = 5;
+    uint256 public constant SILEN = 6;
+    uint256 public constant SOLARY = 7;
+
+    constructor(
+        address adminAddress,
+        uint256[] memory tokenIds,
+        uint256[] memory amounts
+    ) ERC1155("https://hub.qubixinfinity.io/contracts/{id}.json") {
+
+        _mint(msg.sender, BLOODLOV, 1024, "");
+        _mint(msg.sender, LOMIX, 1024, "");
+        _mint(msg.sender, BULLER, 1024, "");
+        _mint(msg.sender, GOLDHUNTER, 512, "");
+        _mint(msg.sender, POWERF, 512, "");
+        _mint(msg.sender, SILEN, 256, "");
+        _mint(msg.sender, SOLARY, 32, "");
+
+        safeBatchTransferFrom(
+            msg.sender,
+            adminAddress,
+            tokenIds,
+            amounts,
+            ""
+        );
+    }
+
+    function uri(uint256 _tokenId) override public pure returns (string memory) {
+        return string(abi.encodePacked(
+                "https://hub.qubixinfinity.io/contracts/",
+                Strings.toString(_tokenId),
+                ".json"
+            ));
+    }
+}
