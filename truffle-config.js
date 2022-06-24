b2548aa5ef854aca066ca4653a10d691f6fb2899
quanter-prog <quanter.box@gmail.com>
2022-06-24 18:46:05 +0300

    fix: config refactoring
    Signed-off-by: quanter-prog <quanter.box@gmail.com>

--- truffle-config.js
+++ truffle-config.js
@@ -52,7 +52,7 @@ module.exports = {
             // ),
             host: "127.0.0.1",     // Localhost (default: none)
             port: 8545,            // Standard Ethereum port (default: none)
-            network_id: "*",       // Any network (default: none)
+            network_id: "*"       // Any network (default: none)
         },
         testnet: {
             provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
@@ -60,7 +60,7 @@ module.exports = {
             confirmations: 10,
             timeoutBlocks: 800,
             skipDryRun: true
-        },
+        }
         // Another network with more advanced options...
         // advanced: {
         // port: 8777,             // Custom port
@@ -96,7 +96,7 @@ module.exports = {
     // Configure your compilers
     compilers: {
         solc: {
-            version: "0.8.1",    // Fetch exact version from solc-bin (default: truffle's version)
+            version: "0.8.1"    // Fetch exact version from solc-bin (default: truffle's version)
             // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
             // settings: {          // See the solidity docs for advice about optimization and evmVersion
             //  optimizer: {
@@ -106,7 +106,7 @@ module.exports = {
             //  evmVersion: "byzantium"
             // }
         }
-    },
+    }
 
     // Truffle DB is currently disabled by default; to enable it, change enabled:
     // false to enabled: true. The default storage location can also be
